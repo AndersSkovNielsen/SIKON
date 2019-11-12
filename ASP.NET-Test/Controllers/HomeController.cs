2ee9a4a49a607cc63acb351672ac4e2d8483a58b
Frederik Sthen Hansen <fred305c@edu.easj.dk>
2019-11-12 10:52:29 +0100

    test af modelclasse

--- ASP.NET-Test/Controllers/HomeController.cs
+++ ASP.NET-Test/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ASP.NET_Test.Models;
 
 namespace ASP.NET_Test.Controllers
 {
@@ -29,7 +30,7 @@ namespace ASP.NET_Test.Controllers
 
         public ActionResult Test()
         {
-            return View();
+            return View(new lecture());
         }
 
         public ActionResult Test2()
---
+++ ASP.NET-Test/Models/lecture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Web;
+
+namespace ASP.NET_Test.Models
+{
+    public class lecture
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public lecture()
+        {
+            _name = "stimming";
+        }
+    }
+}
