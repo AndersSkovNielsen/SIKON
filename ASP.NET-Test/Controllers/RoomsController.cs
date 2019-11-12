b3bfa7f225a0f8727442283b9d220c9409db7aaf
AndersSkovNielsen <anderssnielsen@live.dk>
2019-11-12 10:52:20 +0100

    Room test af MVC.

---
+++ ASP.NET-Test/Controllers/RoomsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using ASP.NET_Test.Models;
+
+namespace ASP.NET_Test.Controllers
+{
+    public class RoomsController : Controller
+    {
+        // GET: Rooms
+        public ActionResult Random()
+        {
+            var room = new Room() {Id = 1, Name = "Hall"};
+            return View(room);
+        }
+    }
+}
---
+++ ASP.NET-Test/Models/Room.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_Test.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
