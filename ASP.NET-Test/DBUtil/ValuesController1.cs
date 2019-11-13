982537a3e9f9dc405dfd67d4e8368326b42d47f1
Frederik Sthen Hansen <fred305c@edu.easj.dk>
2019-11-13 13:21:36 +0100

    db test

---
+++ ASP.NET-Test/DBUtil/ValuesController1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace ASP.NET_Test.DBUtil
+{
+    public class ValuesController1 : ApiController
+    {
+        Managetest mymanager= new Managetest();
+        
+        // GET api/<controller>
+        public string GetTest()
+        {
+            return mymanager.GetAllGuest();
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
